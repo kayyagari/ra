12404f8e040ce34f4b03e0a02888c0a6eb5c5d57
Kiran A <kayyagari@apache.org>
2021-12-31 07:58:23 +0100

    made the add function a method

--- src/rapath/engine.rs
+++ src/rapath/engine.rs
@@ -37,22 +37,24 @@ impl EvalError {
     }
 }
 
-pub fn add<'a>(lhs: Rc<SystemType<'a>>, rhs: Rc<SystemType<'a>>) -> Result<Rc<SystemType<'a>>, EvalError> {
-    match &*lhs {
-        SystemType::String(s) => {
-            let r = rhs.get_as_string().unwrap();
-            let s = format!("{}{}", s, r);
-            Ok(Rc::new(SystemType::String(s)))
-        },
-        SystemType::Number(n) => {
-            let l = n.as_i64();
-            let r = rhs.get_as_number().unwrap();
-            let sd = l + r;
-            let sd = SystemNumber::new_integer(sd);
-            Ok(Rc::new(SystemType::Number(sd)))
-        }
-        _ => {
-            Err(EvalError::from_str("unsupported data type"))
+impl<'a> SystemType<'a> {
+    pub fn add(&self, rhs: Rc<SystemType<'a>>) -> Result<Rc<SystemType<'a>>, EvalError> {
+        match self {
+            SystemType::String(s) => {
+                let r = rhs.get_as_string().unwrap();
+                let s = format!("{}{}", s, r);
+                Ok(Rc::new(SystemType::String(s)))
+            },
+            SystemType::Number(n) => {
+                let l = n.as_i64();
+                let r = rhs.get_as_number().unwrap();
+                let sd = l + r;
+                let sd = SystemNumber::new_integer(sd);
+                Ok(Rc::new(SystemType::Number(sd)))
+            }
+            _ => {
+                Err(EvalError::from_str("unsupported data type"))
+            }
         }
     }
 }
@@ -65,7 +67,7 @@ impl<'a> Ast<'a> {
                   Plus => {
                       let lr = lhs.eval(base)?;
                       let rr = rhs.eval(base)?;
-                      add(lr,rr)
+                      lr.add(rr)
                   },
                   _ => {
                       Err(EvalError::from_str("unsupported binary operation"))
